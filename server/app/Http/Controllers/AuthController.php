54ad0070d0b59873e7dd5d6ecc8bd5ecb3715a7c
Michał <64414824+MichalFurman@users.noreply.github.com>
2021-06-30 17:30:28 +0200

    Update AuthController.php
    Update methods:
    - getAuthenticatedUser - metod return user data and status code or error message and status code,
    - checkAuthenticatedUser - metod return specific data and status code or error message and status code,

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App;
 use App\Services\AuthService;
 use Illuminate\Http\Request;
+use App\Http\Requests\AuthRequest;
 
 
 class AuthController extends Controller
@@ -12,7 +13,8 @@ class AuthController extends Controller
     function __construct() {
     }
    
-    public function login(Request $request, AuthService $authService) {
+    public function login(Request $request, AuthService $authService) 
+    {
         $result = $authService->login($request);
         if ($result['code'] === 200) 
             return response()->json(['token' => $result['token'], 'secret' => $result['secret']],$result['code']);
@@ -20,7 +22,9 @@ class AuthController extends Controller
             return response()->json(['message' => $result['message']],$result['code']);
     }
 
-    public function logout(Request $request, AuthService $authService) {
+    
+    public function logout(Request $request, AuthService $authService) 
+    {
         $result = $authService->logout($request);
         if ($result['code'] === 200) 
             return response()->json(['message' => $result['message']],$result['code']);
@@ -28,14 +32,24 @@ class AuthController extends Controller
             return response()->json(['message' => $result['message']],$result['code']);
     }
 
-    public function getAuthenticatedUser(Request $request, AuthService $authService) {
-        $user =  $authService->authJWTUser($request);
-        return response()->json(compact('user','user'));
+    
+    public function getAuthenticatedUser(Request $request, AuthService $authService) 
+    {
+        $result =  $authService->authJWTUser($request);
+        if ($result['code'] === 200) 
+            return response()->json(['user' => $result['user']],$result['code']);
+        else 
+            return response()->json(['message' => $result['message']],$result['code']);
     }
 
-    public function checkAuthenticatedUser(Request $request, AuthService $authService) {
-        $user =  $authService->authJWTUser($request);
-        return response()->json(compact('user','user'));
+    
+    public function checkAuthenticatedUser(Request $request, AuthService $authService) 
+    {
+        $result =  $authService->authJWTUser($request);
+        if ($result['code'] === 200) 
+            return response()->json(['user' => $result['user']],$result['code']);
+        else 
+            return response()->json(['message' => $result['message']],$result['code']);
     }
 
 }
