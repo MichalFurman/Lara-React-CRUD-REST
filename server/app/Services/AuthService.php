71cf468421be2908c767fb0cdbb7eb566a671e07
Michał <64414824+MichalFurman@users.noreply.github.com>
2021-06-30 17:34:11 +0200

    Update AuthService.php
    Update methods in AuthService:
    - add exceptions for server code 500,

--- server/app/Services/AuthService.php
+++ server/app/Services/AuthService.php
@@ -3,16 +3,19 @@
 namespace App\Services;
 
 use App;
-
+use App\Models\User;
 use Illuminate\Http\Request;
-// use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\AuthRequest;
 
-use Tymon\JWTAuth\Facades\JWTAuth;
-use Tymon\JWTAuth\Facades\JWTFactory;
-use Tymon\JWTAuth\Contracts\JWTSubject;
-use Tymon\JWTAuth\PayloadFactory;
-use Tymon\JWTAuth\JWTManager as JWT;
+// use Tymon\JWTAuth\Facades\JWTFactory;
+// use Tymon\JWTAuth\Contracts\JWTSubject;
+// use Tymon\JWTAuth\PayloadFactory;
+// use Tymon\JWTAuth\JWTManager as JWT;
 
+use Illuminate\Database\QueryException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class AuthService implements iAuthService
 {
@@ -21,14 +24,16 @@ class AuthService implements iAuthService
 
     private $secret = '12345';
     
-    public function authJWToken (Request $request) :array{
+
+    public function authJWToken (Request $request) :array
+    {
         try {
             $token= str_replace('Bearer ', "" , $request->header('Authorization'));
             
-            if (!JWTAuth::setToken($token)) // <-- Check token by setting
+            if (!\JWTAuth::setToken($token)) // <-- Check token by setting
                 return array('message' => 'user_not_found', 'code' => 401);
             
-            if (!$claim = JWTAuth::getPayload()) {
+            if (!$claim = \JWTAuth::getPayload()) {
                 return array('message' => 'user_not_found', 'code' => 401);
             }
             if ($this->secret !== $request->header('Secret')){
@@ -36,93 +41,125 @@ class AuthService implements iAuthService
             }
             return array('message' => 'user_logged', 'code' => 200);
         }
-        catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+        catch (TokenExpiredException $e) {
             return array('message' => 'token_expired', 'code' => 401);
         }
-        catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+        catch (TokenInvalidException $e) {
             return array('message' => 'token_invalid', 'code' => 401);
         }
-        catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+        catch (JWTException $e) {
             return array('message' => 'token_absent', 'code' => 401);
+        }      
+        catch (\Exception $e) {
+            return array('message' => 'something_wrong', 'code' => 500);
         }      
     }
 
-    public function authJWTUser (Request $request){
+
+    public function authJWTUser (Request $request)
+    {
         try {
-            if (!$user = JWTAuth::parseToken()->authenticate()){
+            if (!$user = \JWTAuth::parseToken()->authenticate()){
                 return array('message' => 'user_not_found', 'code' => 401);
             }
             if ($this->secret !== $request->header('Secret')){
                 return array('message' => 'user_not_found', 'code' => 401);
             }
-            return $user;
+            return array('user' =>$user, 'code' =>200);
         }
-        catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+        catch (TokenExpiredException $e) {
             return array('message' => 'token_expired', 'code' => 401);
         }
-        catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+        catch (TokenInvalidException $e) {
             return array('message' => 'token_invalid', 'code' => 401);
         }
-        catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+        catch (JWTException $e) {
             return array('message' => 'token_absent', 'code' => 401);
+        }
+        catch (\Exception $e) {
+            return array('message' => 'something_wrong', 'code' => 500);
         }      
     }
 
 
-    public function authJWTUserForm (){
+    public function authJWTUserForm ()
+    {
         try {
-            if (!$user = JWTAuth::parseToken()->authenticate()){
+            if (!$user = \JWTAuth::parseToken()->authenticate()){
                 return array('message' => 'user_not_found', 'code' => 401);
             }
             return $user;
         }
-        catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+        catch (TokenExpiredException $e) {
             return array('message' => 'token_expired', 'code' => 401);
         }
-        catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+        catch (TokenInvalidException $e) {
             return array('message' => 'token_invalid', 'code' => 401);
         }
-        catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+        catch (JWTException $e) {
             return array('message' => 'token_absent', 'code' => 401);
+        }      
+        catch (\Exception $e) {
+            return array('message' => 'something_wrong', 'code' => 500);
         }      
     }
 
-    public function login(Request $request) : array {
+
+    public function login(Request $request) : array 
+    {
         $credentials = $request->json()->all();
         $secret =  $this->secret;
 
         try {
-            if (!$token = JWTAuth::attempt($credentials)){
+            if (!$token = \JWTAuth::attempt($credentials)){
                 return array('message' => 'invalid_credentials','code' =>401);
             }
+        }
+        catch (\ErrorException $e){
+            return array('message' => 'invalid_credentials','code' =>401);
+        }
+        catch (QueryException $e){
+            return array('message' => 'invalid_credentials','code' =>401);
         }
         catch (JWTException $e){
             return array('message' => 'could_not_create_token','code' =>500);
         }
+        catch (\Exception $e) {
+            return array('message' => 'something_wrong','code' => 500);
+        }      
         return array('token'=>$token, 'secret'=>$secret, 'code'=>200);
     }
 
-    public function logout(Request $request) : array {
+
+    public function logout(Request $request) : array 
+    {
         $credentials = $request->json()->all();
 
         try {
-            if (!$token = JWTAuth::attempt($credentials)){
+            if (!$token = \JWTAuth::attempt($credentials)){
                 return array('message' => 'invalid_credentials','code' =>401);
             }
-            JWTAuth::invalidate($token);
+            \JWTAuth::invalidate($token);
         }
         catch (JWTException $e){
             return array('message' => 'could_not_create_token','code' =>500);
         }
+        catch (\Exception $e) {
+            return array('message' => 'something_wrong', 'code' => 500);
+        }      
         return array('message'=>'logout_successful', 'code'=>200);
     }
 
-    public function AuthenticatedUser(Request $request) {
+
+    public function AuthenticatedUser(Request $request) 
+    {
         $user = $this->authJWTUser($request);
         return $user;
     }
 
-    public function getToken(User $user) {
-        return JWTAuth::fromUser($user);
+
+    public function getToken(User $user) 
+    {
+        return \JWTAuth::fromUser($user);
     }
 }
